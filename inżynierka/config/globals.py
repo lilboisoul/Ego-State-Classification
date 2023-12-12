573635d8be7db81a7a29166e84a2f748680e749c
lilboisoul <67749413+lilboisoul@users.noreply.github.com>
2023-12-12 22:18:10 +0100

    Config update

--- inżynierka/config/globals.py
+++ inżynierka/config/globals.py
@@ -1,23 +1,37 @@
 import datetime
 import os
 
+import pandas as pd
 import torch.cuda
+from transformers import BertTokenizer
 
 # -----------------LABELS--------------------
-labels = ["NP", "CP", "A", "FC", "AC"]
+#labels = ["NP", "CP", "A", "FC", "AC"] #functional
+labels = ["P", "A", "C"] #structural
 num_labels = len(labels)
 id2label = {i: label for i, label in enumerate(labels)}
 label2id = {val: key for key, val in id2label.items()}
 current_date = datetime.datetime.now()
 # -------------------------------------------
-NUM_FOLDS = 8
-NUM_EPOCHS = 3
+NUM_FOLDS = 5
+NUM_EPOCHS = 4
 BATCH_SIZE = 16
+#BASE_MODEL = 'bert-base-cased' TO CHECK
+#TOKENIZER = BertTokenizer.from_pretrained('bert-base-cased')
+BASE_MODEL = 'bert-base-uncased'
+TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
 
+#CURRENT_MODEL = Bert
+#TRAIN_DATASET =
+#TEST_DATASET =
+#VAL_DATASET =
 
-if torch.cuda.is_available():
-    device = 'cuda'
-    print(f"Using: {torch.cuda.get_device_name(0)}")
-else:
-    device = 'cpu'
-    print("Using: cpu")
+DEVICE = 'cuda'
+
+def choose_device():
+    if torch.cuda.is_available():
+        DEVICE = 'cuda'
+        print(f"Using: {torch.cuda.get_device_name(0)}")
+    else:
+        DEVICE = 'cpu'
+        print("Using: cpu")
--- inżynierka/config/neural_net.py
+++ inżynierka/config/neural_net.py
@@ -1,28 +1,14 @@
-from config.globals import num_labels, id2label, label2id, device
+from config.globals import num_labels, id2label, label2id, DEVICE
 from transformers import BertTokenizer, BertForSequenceClassification, RobertaForSequenceClassification, \
     RobertaTokenizer, DistilBertTokenizer, DistilBertForSequenceClassification, DistilBertConfig
 
 base_model = 'bert-base-uncased'
-base_model2 = 'roberta-base'
-#base_model3 = 'distilbert-base-uncased'
+
 bert_model = BertForSequenceClassification.from_pretrained(base_model, num_labels=num_labels, id2label=id2label,
-                                                      label2id=label2id)
+                                                           label2id=label2id)
 bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-#roberta_tokenizer = RobertaTokenizer.from_pretrained(base_model2)
-#roberta_model = RobertaForSequenceClassification.from_pretrained(base_model2, num_labels=num_labels, id2label=id2label,label2id=label2id)
-
-if device == 'cuda':
+if DEVICE == 'cuda':
     if bert_model:
         bert_model = bert_model.to('cuda')
-    #roberta_model = roberta_model.to('cuda')
 
-
-metrics = ""  # [
-# smp.utils.metrics.IoU(threshold=0.5),
-# smp.utils.metrics.Fscore(threshold=0.5),
-# smp.utils.metrics.Accuracy(threshold=0.5),
-# smp.utils.metrics.Recall(threshold=0.5),
-# smp.utils.metrics.Precision(threshold=0.5),]
-
-optimizer = ""  # torch.optim.Adam([dict(params=model.parameters(), lr=lr),)]
